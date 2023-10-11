6bb451f7b5e0c71f071c1d401f12d0634a62352c
Tridip Bhowmik <tridipbhowmik898@gmail.com>
2023-10-11 23:40:25 +0600

    calendar

--- lib/main.dart
+++ lib/main.dart
@@ -13,86 +13,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CalendarControllerProvider(
-      controller: EventController()..addAll(events),
-      child: GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/',
-        getPages: [
-          GetPage(
-            name: '/',
-            page: () => HomePage(),
-          ),
-        ],
-      ),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => HomePage(),
+        ),
+      ],
     );
   }
 }
-List<CalendarEventData<Event>> events = [
-  CalendarEventData(
-    date: DateTime.now(),
-    event: const Event(title: "Joe's Birthday"),
-    title: "Project meeting",
-    description: "Today is project meeting.",
-    startTime: DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 18, 30),
-    endTime: DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 22),
-  ),
-  CalendarEventData(
-    date: DateTime.now().add(const Duration(days: 1)),
-    startTime: DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 18),
-    endTime: DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 19),
-    event: const Event(title: "Wedding anniversary"),
-    title: "Wedding anniversary",
-    description: "Attend uncle's wedding anniversary.",
-  ),
-  CalendarEventData(
-    date: DateTime.now(),
-    startTime: DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 14),
-    endTime: DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 17),
-    event: const Event(title: "Football Tournament"),
-    title: "Football Tournament",
-    description: "Go to football tournament.",
-  ),
-  CalendarEventData(
-    date: DateTime.now().add(const Duration(days: 3)),
-    startTime: DateTime(DateTime.now().add(const Duration(days: 3)).year,
-        DateTime.now().add(const Duration(days: 3)).month, DateTime.now().add(Duration(days: 3)).day, 10),
-    endTime: DateTime(DateTime.now().add(const Duration(days: 3)).year,
-        DateTime.now().add(const Duration(days: 3)).month, DateTime.now().add(Duration(days: 3)).day, 14),
-    event: const Event(title: "Sprint Meeting."),
-    title: "Sprint Meeting.",
-    description: "Last day of project submission for last year.",
-  ),
-  CalendarEventData(
-    date: DateTime.now().subtract(const Duration(days: 2)),
-    startTime: DateTime(
-        DateTime.now().subtract(const Duration(days: 2)).year,
-        DateTime.now().subtract(const Duration(days: 2)).month,
-        DateTime.now().subtract(const Duration(days: 2)).day,
-        14),
-    endTime: DateTime(
-        DateTime.now().subtract(Duration(days: 2)).year,
-        DateTime.now().subtract(Duration(days: 2)).month,
-        DateTime.now().subtract(Duration(days: 2)).day,
-        16),
-    event: Event(title: "Team Meeting"),
-    title: "Team Meeting",
-    description: "Team Meeting",
-  ),
-  CalendarEventData(
-    date: DateTime.now().subtract(Duration(days: 2)),
-    startTime: DateTime(
-        DateTime.now().subtract(Duration(days: 2)).year,
-        DateTime.now().subtract(Duration(days: 2)).month,
-        DateTime.now().subtract(Duration(days: 2)).day,
-        10),
-    endTime: DateTime(
-        DateTime.now().subtract(Duration(days: 2)).year,
-        DateTime.now().subtract(Duration(days: 2)).month,
-        DateTime.now().subtract(Duration(days: 2)).day,
-        12),
-    event: Event(title: "Chemistry Viva"),
-    title: "Chemistry Viva",
-    description: "Today is Joe's birthday.",
-  ),
-];
